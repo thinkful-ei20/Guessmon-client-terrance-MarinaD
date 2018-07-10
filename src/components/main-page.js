8458d64d9212ed9e916377c05df3ce51a49aadf8
Terrance Corley <terrancecorley@outlook.com>
2018-07-10 11:22:48 -0700

    refactoring boilerplate

---
+++ src/components/main-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Just testing this out.</p>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
