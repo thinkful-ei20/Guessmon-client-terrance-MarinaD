60370195247d3c06104ac847de9ba781f4695f99
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-10 15:14:13 -0600

    Add actions and reducers

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import {loadAuthToken} from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
+import questionReducer from './reducers/questions';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -12,7 +13,8 @@ const store = createStore(
   combineReducers({
     form: formReducer,
     auth: authReducer,
-    protectedData: protectedDataReducer
+    protectedData: protectedDataReducer,
+    data : questionReducer
   }),
   composeEnhancers(applyMiddleware(thunk))
 );
