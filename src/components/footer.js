98038611c000c487860b738d5496eb83d4463c7a
Terrance Corley <terrancecorley@outlook.com>
2018-07-12 14:05:37 -0700

    added base styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 export function MainFooter() {
   return (
     <footer className="main-footer">
-      <p>&copy; Made with <span className="heart">&hearts;</span> by <a href="https://mdargitz.com">Marina</a> &amp; <a href="https://terrancecorley.com">Terrance</a></p>
+      <p>&copy; Made with <span className="heart">&hearts;</span> by gym bosses <a className="link-effect" href="https://mdargitz.com">Marina</a> &amp; <a className="link-effect" href="https://terrancecorley.com">Terrance</a></p>
     </footer>
   );
 }
