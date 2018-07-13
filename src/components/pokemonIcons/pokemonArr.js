498fc532b24433df943bf460e03a8131b27c2293
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-13 08:16:17 -0600

    Add buddy system

---
+++ src/components/pokemonIcons/pokemonArr.js
@@ -0,0 +1,97 @@
+import React from 'react';
+
+import Pikachu from './Pikachu';
+import Abra from './Abra';
+import Bellsprout from './Bellsprout';
+import Bullbasaur from './Bullbasaur';
+import Caterpie from './Caterpie';
+import Charmander from './Charmander';
+import Dratini from './Dratini';
+import Eevee from './Eevee';
+import Jigglypuff from './Jigglypuff';
+import Mankey from './Mankey';
+import Meowth from './Meowth';
+import Mew from './Mew';
+import Psyduck from './Psyduck';
+import Rattata from './Rattata';
+import Snorlax from './Snorlax';
+import Squirtle from './Squirtle';
+import Venonat from './Venonat';
+import Weedle from './Weedle';
+
+const pokemonArr = [
+  {
+    icon: <Pikachu />,
+    value: 'Pikachu'
+  },
+  {
+    icon: <Abra />,
+    value: 'Abra'
+  },
+  {
+    icon: <Bellsprout />,
+    value: 'Bellsprout'
+  },
+  {
+    icon: <Bullbasaur />,
+    value: 'Bullbasaur'
+  },
+  {
+    icon: <Caterpie />,
+    value: 'Caterpie'
+  },
+  {
+    icon: <Charmander />,
+    value: 'Charmander'
+  },
+  {
+    icon: <Dratini />,
+    value: 'Dratini'
+  },
+  {
+    icon: <Eevee />,
+    value: 'Eevee'
+  },
+  {
+    icon: <Jigglypuff />,
+    value: 'Jigglypuff'
+  },
+  {
+    icon: <Mankey />,
+    value: 'Mankey'
+  },
+  {
+    icon: <Meowth />,
+    value: 'Meowth'
+  },
+  {
+    icon: <Psyduck />,
+    value: 'Psyduck'
+  },
+  {
+    icon: <Rattata />,
+    value: 'Rattata'
+  },
+  {
+    icon: <Snorlax />,
+    value: 'Snorlax'
+  },
+  {
+    icon: <Squirtle />,
+    value: 'Squirtle'
+  },
+  {
+    icon: <Venonat />,
+    value: 'Venonat'
+  },
+  {
+    icon: <Weedle />,
+    value: 'Weedle'
+  },
+  {
+    icon: <Mew />,
+    value: 'Mew'
+  }
+];
+
+export default pokemonArr;
